3d3418c5753cb827981a0b76e70b1cea3fce2c98
ElVichoSiu <vthielemunoz@gmail.com>
2024-09-26 15:47:15 -0300

    FEAT: Creacion del frontend inicial, acelerometro que reacciona a cambio de velocidades

--- eolian-visualizador/src/App.js
+++ eolian-visualizador/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="speedometer">
+          <div className="needle" style={{ transform: `rotate(${(100-50)*1.8}deg)`}}>
+            <spam className="score">0</spam>
+          </div>
+        </div>
       </header>
     </div>
   );
